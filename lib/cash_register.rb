1f3c84cfcc569a460c1937ac4fd46ed17cb1c165
favorable-device-5158 <blee2125@gmail.com>
2019-09-11 13:36:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+  attr_accessor :total
 
+  def initialize(total)
+    @total = total
+  end
+
+  def add_item
+    
+  end
+
+  def apply_discount
+    
+  end
+
+  def items
+    
+  end
+
+  def void_last_transaction
+    
+  end
+
+end
